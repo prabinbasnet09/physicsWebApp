f32320d1dd3cee2ab93d783b5fd418cf2e5ad046
prabin <prabinbasnet09@gmail.com>
2021-11-11 15:28:30 -0600

    Values retried by their id instead of class in Hoomepage.html

--- calculation.js
+++ calculation.js
@@ -2,6 +2,10 @@ const g = 9.8;
 let height, time, initialVelocity;
 let velocity = 5.0;
 
-function calcHeight(gravity, velocity, time){
+
+function calcHeight(gravity, velocity, height, time){
     height = velocity*time - 0.5 * gravity * Math.pow(time, 2);
+    console.log(height);
 }
+
+
